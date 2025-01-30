9654c4021fa7a27c06845e01beb569dbd0e19fed
bepoisso <poisson.benjamin69@gmail.com>
2025-01-30 13:47:41 +0100

    Update initilisation

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 18:56:57 by bepoisso          #+#    #+#             */
-/*   Updated: 2025/01/28 16:51:11 by bepoisso         ###   ########.fr       */
+/*   Updated: 2025/01/30 13:44:19 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	philo(t_data *data)
 
 	fork = init_fork(data);
 	philo = init_philo(data, fork);
+	inti_data(data, philo, fork);
 	simulation(data, philo);
 	free_philo(&philo);
 	free_fork(fork, data->philo_nbr);
