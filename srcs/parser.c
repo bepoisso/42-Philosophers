4fbb197f1fa555b57cc76c509041692f5134930f
bepoisso <poisson.benjamin69@gmail.com>
2025-01-30 13:46:30 +0100

    add secur passing

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: bepoisso <bepoisso@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 18:41:20 by bepoisso          #+#    #+#             */
-/*   Updated: 2025/01/25 14:59:29 by bepoisso         ###   ########.fr       */
+/*   Updated: 2025/01/30 13:40:49 by bepoisso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,28 @@
 // ./philo nbr_philo time_to_die time_to_eat time_to_sleep [max_meals]
 void	parser(t_data *data, int ac, char **av)
 {
-	data->philo_nbr = ft_atoi(av[1]);
-	data->time_to_die = ft_atoi(av[2]);
-	data->time_to_eat = ft_atoi(av[3]);
-	data->time_to_sleep = ft_atoi(av[4]);
+	if (is_not_correct(av[1]))
+		ft_perror("Error\nNbr of philosophers is not correct\n");
+	data->philo_nbr = ft_atoll(av[1]);
+	if (is_not_correct(av[2]))
+		ft_perror("Error\nTime_to_die is not correct\n");
+	data->time_to_die = ft_atoll(av[2]) * 1e3;
+	if (is_not_correct(av[3]))
+		ft_perror("Error\nTime_to_eat is not correct\n");
+	data->time_to_eat = ft_atoll(av[3]) * 1e3;
+	if (is_not_correct(av[4]))
+		ft_perror("Error\nTime_to_sleep is not correct\n");
+	data->time_to_sleep = ft_atoll(av[4]) * 1e3;
+	if (data->time_to_die < 6e4
+		|| data->time_to_eat < 6e4
+		|| data->time_to_sleep < 6e4)
+		ft_perror("Error\nPlease use timestamp major than 60ms\n");
 	if (ac == 6)
-		data->max_meals = ft_atoi(av[5]);
+	{
+		if (is_not_correct(av[5]))
+			ft_perror("Error\nMax_meals is not correct\n");
+		data->max_meals = ft_atoll(av[5]);
+	}
+	else
+		data->max_meals = -1;
 }
